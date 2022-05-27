72e05bcda702495fe2317e4fac83293201707478
Torotonn <totoro3225@gmail.com>
2022-05-27 19:56:08 +0900

    一時退避

--- gen_loot_tables/data/update_blockstates.py
+++ gen_loot_tables/data/update_blockstates.py
@@ -8,12 +8,14 @@ DIR_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
 BOOLEAN = [False, True]
 DIT_4 = ["east", "north", "south", "west"]
 DIT_6 = ["east", "north", "south", "west", "up", "down"]
+XYZ = ["x", "y", "z"]
 
 
 # update
 def update_dict(keys: list, values: list, id: set):
+    id_ = 'minecraft:' + id
     s_dict = dict(zip(keys, values))
-    dic = {k: s_dict for k in id}
+    dic = {k: s_dict for k in id_}
     blockstates.update(dic)
 
     return
@@ -35,13 +37,154 @@ def set_id(block_tags: set, except_id: set = set()):      # (block tag name[set]
 blockstates = json.loads((DIR_PATH / 'blockstates.json').read_text())
 
 # version
-blockstates.update(version='1.18.2')
+blockstates.update(version='1.19')
 
-# update
-#   keys:list = ["facing", "waterlogged"]
-#   values:list = [DIT_4, [False, True]]
-#   id:set = {"minecraft:foo"}
-#   update_dict(keys, values, id)
+# 1.19 update
+# froglight
+keys = ["axis"]
+values = [XYZ]
+id = {"pearlescent_froglight", "verdant_froglight", "ochre_froglight"}
+update_dict(keys, values, id)
+
+# leaves
+keys = ['distance', 'persistent', 'waterlogged']
+values = [list(range(1, 8)), BOOLEAN, BOOLEAN]
+id = set_id({'leaves'})
+update_dict(keys, values, id)
+
+# mangrove_logs
+keys = ["axis"]
+values = [XYZ]
+id = set_id('mangrove_logs')
+update_dict(keys, values, id)
+
+# mangrove_slab
+keys = ["type", "waterlogged"]
+values = [["bottom", "top", "double"], BOOLEAN]
+id = {"mangrove_slab"}
+update_dict(keys, values, id)
+
+# mangrove_stair
+keys = ["facing", "half", "shape", "waterlogged"]
+values = [DIT_4, ["bottom", "top"], ["inner_left", "inner_right", "outer_left", "outer_right", "straight"], BOOLEAN]
+id = {"mangrove_stairs"}
+update_dict(keys, values, id)
+
+# mangrove_fence
+keys = ["east", "north", "south", "waterlogged", "west"]
+values = [BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN]
+id = {"mangrove_fence"}
+update_dict(keys, values, id)
+
+# mangrove_fence_gate
+keys = ["facing", "in_wall", "open", "powered"]
+values = [DIT_4, BOOLEAN, BOOLEAN, BOOLEAN]
+id = {"mangrove_fence_gate"}
+update_dict(keys, values, id)
+
+# mangrove_plate
+keys = ["powered"]
+values = [BOOLEAN]
+id = {"mangrove_pressure_plate"}
+update_dict(keys, values, id)
+
+# mangrove_sign
+keys = ["rotation", "lit", "waterlogged"]
+values = [list(range(1, 16)), BOOLEAN, BOOLEAN]
+id = {"mangrove_sign", "mangrove_wall_sign"}
+update_dict(keys, values, id)
+
+# mangrove_button
+keys = ["face", "facing", "powered"]
+values = [["ceiling", "floor", "wall"], DIT_4, BOOLEAN]
+id = {"mangrove_button"}
+update_dict(keys, values, id)
+
+# mangrove_door
+keys = ["facing", "half", "hinge", "open", "powered"]
+values = [DIT_4, ["lower", "upper"], ["left", "right"], BOOLEAN, BOOLEAN]
+id = {"mangrove_door"}
+update_dict(keys, values, id)
+
+# mangrove_td
+keys = ["facing", "half", "open", "powered", "waterlogged"]
+values = [DIT_4, ["bottom", "top"], BOOLEAN, BOOLEAN, BOOLEAN]
+id = {"mangrove_trapdoor"}
+update_dict(keys, values, id)
+
+# mangrove_progagule
+keys = ["age", "hanging", "stage", "waterlogged"]
+values = [list(range(4)), BOOLEAN, [0, 1], BOOLEAN]
+id = {"mangrove_progagule"}
+update_dict(keys, values, id)
+
+# mangrove_roots
+keys = ["waterlogged"]
+values = [BOOLEAN]
+id = {"mangrove_roots"}
+update_dict(keys, values, id)
+
+# mud_brick_slab
+keys = ["type", "waterlogged"]
+values = [["bottom", "top", "double"], BOOLEAN]
+id = {"mud_brick_slab"}
+update_dict(keys, values, id)
+
+# mud_brick_stairs
+keys = ["facing", "half", "shape", "waterlogged"]
+values = [DIT_4, ["bottom", "top"], ["inner_left", "inner_right", "outer_left", "outer_right", "straight"], BOOLEAN]
+id = {"mud_brick_stairs"}
+update_dict(keys, values, id)
+
+# mud_brick_wall # wall
+keys = ["east", "north", "south", "waterlogged", "west", "up"]
+values = [BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN]
+id = {"mud_brick_wall"}
+update_dict(keys, values, id)
+
+# muddy_mangrove_roots
+keys = ["axis"]
+values = [XYZ]
+id = {"muddy_mangrove_roots"}
+update_dict(keys, values, id)
+
+# sculk_catalyst
+keys = ["bloom"]
+values = [BOOLEAN]
+id = {"sculk_catalyst"}
+update_dict(keys, values, id)
+
+# sculk_shrieker
+keys = ["can_summon", "shrieking", "waterlogged"]
+values = [BOOLEAN, BOOLEAN, BOOLEAN]
+id = {"sculk_shrieker"}
+update_dict(keys, values, id)
+
+# sculk_vein
+keys = ["east", "north", "south", "waterlogged", "west", "up", "down"]
+values = [BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN]
+id = {"sculk_vein"}
+update_dict(keys, values, id)
+
+keys = []
+values = []
+id = {}
+update_dict(keys, values, id)
+
+keys = []
+values = []
+id = {}
+update_dict(keys, values, id)
+
+keys = []
+values = []
+id = {}
+update_dict(keys, values, id)
+
+keys = []
+values = []
+id = {}
+update_dict(keys, values, id)
 
 # export
 print(blockstates)
